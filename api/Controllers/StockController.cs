0e80d004986b9a634cd1b3319823724e2e43c54d
hamada-SOS <mohammadhassan241@gmail.com>
2024-09-28 17:59:08 +0300

    delete

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -49,5 +49,47 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new { id = stockModel.ID }, stockModel.ToStockDto());
         }
 
+        [HttpPut]
+        [Route("{id}")]
+
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.ID == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+
+            }
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.Company = updateDto.Company;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Indestry = updateDto.Indestry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.ID == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
     }
 }
